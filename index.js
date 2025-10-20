8a530613eb140e39d20b1a225e194eceea6fbcb2
Nae-Coleman <teranaecoleman@gmail.com>
2025-10-20 14:17:14 -0400

    Block12 workshop

---
+++ index.js
@@ -0,0 +1,23 @@
+let code1;
+code1 = 7 + 3;
+// console.log(code1);
+
+let code2;
+code2 = (100 - 20) / 2;
+// console.log(code2);
+
+let code3;
+code3 = (3 % 2) + 38;
+// console.log(code3);
+
+let message = "The vault has been secured. The combination is: ";
+// console.log(message);
+
+let codeA;
+codeA = code1 + "-" + code2 + "-" + code3;
+// console.log(codeA);
+
+let codeb;
+codeb = `${10}-${40}-${39}`;
+console.log(message + codeA);
+console.log(message + codeb);
